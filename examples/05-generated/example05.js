b47a48dc651299742985468a1e5533f085366038
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-07-16 00:26:44 +0200

    Cleanup.

--- examples/05-generated/example05.js
+++ examples/05-generated/example05.js
@@ -2,10 +2,6 @@
 var data = require('./data');
 
 window.addEventListener('load', function() {
-  var navButton = document.querySelector('#nav-button');
-  navButton.addEventListener('click', function() {
-    alert('Clicked on #nav-button');
-  });
   // adding content programmatically
   var pageBody = document.querySelector('.page-body');
   data.forEach(function(text) {
